216b1a9adc717c7e04e64c3ff89e83ef0fef59e1
Mebayonas Yonas <mebayonasyonas@gmail.com>
2024-11-03 16:53:08 +0300

    change on vercle

--- Frontend/src/Utility/ApiConfig.js
+++ Frontend/src/Utility/ApiConfig.js
@@ -1,29 +1,8 @@
 import axios from "axios";
 
-// Set up axios instance with a base URL, using environment variable or default
 const axiosBaseURL = axios.create({
-  baseURL: "https://evangadi-forum2-6.onrender.com"
+  baseURL: "http://localhost:5500/api",
 });
-
-// Adding a request interceptor
-axiosBaseURL.interceptors.request.use(config => {
-  // Modify config before sending the request
-  const token = localStorage.getItem('token'); // Example of retrieving a token
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
-
-// Adding a response interceptor
-axiosBaseURL.interceptors.response.use(response => {
-  return response;
-}, error => {
-  // Handle error responses here
-  return Promise.reject(error);
-});
-
 
 export default axiosBaseURL;
+export const axiosImageURL = "http://localhost:5500/api";
