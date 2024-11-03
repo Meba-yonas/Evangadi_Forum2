41961150aafdf36b6eb687a5591d8154b8ae1c66
Mebayonas Yonas <mebayonasyonas@gmail.com>
2024-11-03 06:57:50 +0300

    change on api

--- Frontend/src/Utility/ApiConfig.js
+++ Frontend/src/Utility/ApiConfig.js
@@ -1,7 +1,29 @@
-import  axios from 'axios'
+import axios from "axios";
 
+// Set up axios instance with a base URL, using environment variable or default
 const axiosBaseURL = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://evangadi-forum2-6.onrender.com"
 });
+
+// Adding a request interceptor
+axiosBaseURL.interceptors.request.use(config => {
+  // Modify config before sending the request
+  const token = localStorage.getItem('token'); // Example of retrieving a token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+// Adding a response interceptor
+axiosBaseURL.interceptors.response.use(response => {
+  return response;
+}, error => {
+  // Handle error responses here
+  return Promise.reject(error);
+});
+
 
 export default axiosBaseURL;
