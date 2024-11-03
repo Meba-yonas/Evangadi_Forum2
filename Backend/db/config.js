2224902b32f57526132c4fc7a721eff904ab9b12
Mebayonas Yonas <mebayonasyonas@gmail.com>
2024-11-03 08:37:16 +0300

    change on Db

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
 const mysql2 = require("mysql2/promise");
 
 const dbconnection = mysql2.createPool({
-  user: process.env.DB_USER || "Evangadi_admin",
-  database: process.env.DB_NAME || "Evangadi_Forum",
-  host: process.env.DB_HOST || "localhost",
-  password: process.env.DB_PASSWORD || "1234567",
+  user:"Evangadi_admin",
+  database: "Evangadi_Forum",
+  host: "localhost",
+  password: "1234567",
   connectionLimit: 10,
-  waitForConnections: true,
-  queueLimit: 0
 });
 
 module.exports = dbconnection;
-
